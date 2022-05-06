483fc6ab77583b9730eb9a9636dbf1a74bbe450c
ErrorGuyWithGun <thevlad3323@gmail.com>
2022-05-06 08:14:44 +0300

    add players and world

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
+using System.IO;
 
 public class PlayerManager : MonoBehaviour
 {
+
+    private PhotonView _photonView;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _photonView = GetComponent<PhotonView>();
+        if (_photonView.IsMine)
+        {
+            CreateController();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CreateController()
     {
-        
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), Vector3.zero, Quaternion.identity);
     }
+    
 }
